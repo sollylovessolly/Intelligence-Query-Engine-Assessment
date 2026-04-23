90a9200a3cf5f1a45ca586b1bf932f46eff8db3e
oyin <oyindamolasolarin@gmail.com>
2026-04-23 10:10:02 +0100

    fix port for railway

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import app from "./app";
 
-app.listen(4000, () => {
-  console.log("Server running on port 4000 🚀");
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT} 🚀`);
 });
